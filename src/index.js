8281378b73228548fc7c8750fe7b65bd4ceb2b82
pradeep7878 <pradivpj@gmail.com>
2024-08-26 14:35:58 +0530

    404-htm-file-updation

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import Home from './Home';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
           <Route path='/' element={<App />} />        
           <Route path='/home' element={<Home/>} />        
